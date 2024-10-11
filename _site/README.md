b31204fbada747700f6b6cdbb386b1c2a4198da1
William Fortin <willyfortin@gmail.com>
2024-10-11 14:21:33 -0400

    New website

--- _site/README.md
+++
@@ -1,2 +0,0 @@
-#WFortin - Personal blog
-=======
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6439FF",
+      },
+    },
+    fontFamily: {
+      display: ["Space Mono", "serif"],
+    },
+  },
+  plugins: [],
+};
